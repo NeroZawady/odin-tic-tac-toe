71e22357c83630ab0305b409ae1858f078735925
Nero Zawady <nerozawady@gmail.com>
2021-12-12 20:11:51 -0900

    Refactor names

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+const gameContainer = document.querySelector("#gameContainer");
+const playerName = document.querySelector("#playerName");
+const score = document.querySelector("#score");
+const form = document.querySelector("form");
+const nameInput = document.querySelector("form input");
+const buttonSubmit = document.querySelector("form button");
 const characterSelectionContainer = document.querySelector("#characterSelectionContainer");
-const buttonSubmit = document.querySelector("#submit");
-const nameInput = document.querySelector("#nameInput");
 const buttonCharacterX = document.querySelector("#characterX");
 const buttonCharacterO = document.querySelector("#characterO");
-const nameForm = document.querySelector("#name");
-const gameContainer = document.querySelector("#gameContainer");
+
+const gameBoard = (() => {
+  const cells = Array.from(document.querySelectorAll("#boardContainer button"));
+
+  return {
+    cells,
+  }
+
+})();
 
 const player = (() => {
   let name = null;
@@ -28,27 +39,27 @@ const computer = (() => {
 
 })();
 
-nameForm.addEventListener("submit", () => {
+form.addEventListener("submit", () => {
   event.preventDefault();
   player.name = nameInput.value;
   nameInput.value = null;
 
-  nameForm.style.display = "none";
+  form.style.display = "none";
   characterSelectionContainer.style.display = "block";
 })
 
 nameInput.addEventListener("keyup", (event) => {
   if(event.keyCode === 13) {
     event.preventDefault();
-    nameForm.submit();
+    form.submit();
   }
 })
 
-buttonCharacterX.addEventListener("click", () => setPlayerComputerCharacters("X", "O"));
+buttonCharacterX.addEventListener("click", () => setPlayerAndComputerCharacters("X", "O"));
 
-buttonCharacterO.addEventListener("click", () => setPlayerComputerCharacters("O", "X"));
+buttonCharacterO.addEventListener("click", () => setPlayerAndComputerCharacters("O", "X"));
 
-function setPlayerComputerCharacters(playerCharacter, computerCharacter) {
+function setPlayerAndComputerCharacters(playerCharacter, computerCharacter) {
   player.character = playerCharacter;
   computer.character = computerCharacter;
 
